38429cbbc7ed0ec5d94bb1c79930a88204e79064
MdJowelAhmed <mdjowelahmed924@gmail.com>
2026-02-21 11:51:08 +0600

    update icon

--- app/components/home/UseUs.tsx
+++ app/components/home/UseUs.tsx
@@ -3,45 +3,45 @@ import { Building, File, IdCard, Phone, User2, Verified } from "lucide-react";
 import WhyUse from "../ui/WhyUse";
 
 const icons = {
-  Verified: <Verified />,
-  IdCard: <IdCard />,
-  Building: <Building />,
-  User2: <User2 />,
-  Phone: <Phone />,
-  File: <File />,
+  Verified: <Verified size={40} />,
+  IdCard: <IdCard size={40}   />,
+  Building: <Building size={40}   />,
+  User2: <User2 size={40}   />,
+  Phone: <Phone size={40}   />,
+  File: <File size={40}   />,
 };
 
 const useUsData = [
   {
     id: 1,
-    icon: Verified,
+    icon: "Verified",
     title: "Verified professionals",
   },
 
 
   {
     id: 2,
-    icon: IdCard,
+    icon: "IdCard",
     title: "ID Checked & vetted trades",
   },
   {
     id: 3,
-    icon: Building,
+    icon: "Building",
     title: "Compare up to 3 quotes",
   },
   {
     id: 4,
-    icon: User2,
+    icon: "User2",
     title: "Local professionals only",
   },
   {
     id: 5,
-    icon: File,
+    icon: "File",
     title: "Trades see full job details upfront",
   },
   {
     id: 6,
-    icon: Phone,
+    icon: "Phone",
     title: "No endless phone calls",
   },
  
