673d661a2bc827ee428629ac17d2744b42d6251d
Mijanur Rahman <mijan.connect@gmail.com>
2026-06-17 10:11:21 +0600

    fix: update styling for service content to support paragraph and list formatting

--- app/components/categories/ServiceDetails.tsx
+++ app/components/categories/ServiceDetails.tsx
@@ -122,7 +122,7 @@ export default function ServiceDetails() {
             <div
               data-aos="fade-up"
               data-aos-delay="100"
-              className="text-[14px] lg:text-[18px] text-primaryTextLight mt-4 lg:mt-10 whitespace-pre-line list-disc list-inside"
+              className="service-content text-[14px] lg:text-[18px]  text-primaryTextLight mt-4 lg:mt-10"
               dangerouslySetInnerHTML={{
                 __html: DOMPurify.sanitize(
                   categoryDetailsData?.servicesDetailsDescription2 || "",
